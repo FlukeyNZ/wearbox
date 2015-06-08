98afae84ce5a0a95bb74225f458e5321edc2f350
Tim Bourne <tim@bourne.co.nz>
2015-06-08 21:36:57 +1200

    Add Default Gearbox Theme

---
+++ addons/shared/themes/wearbox/theme.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Wearbox extends Theme {
+
+    public $name = 'Wearbox';
+    public $author = 'Tim Bourne';
+    public $description = 'www.wearbox.co.nz';
+    public $version = '1.0';
+
+}
+
+/* End of file theme.php */
